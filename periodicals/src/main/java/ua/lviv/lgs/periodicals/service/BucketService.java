22e64a0ac2740d4e40a7603ad2238c46a82fc10a
NestorPavlenko <88089857+NestorPavlenko@users.noreply.github.com>
2022-06-21 23:52:13 +0300

    added loggers to Service methods

--- periodicals/src/main/java/ua/lviv/lgs/periodicals/service/BucketService.java
+++ periodicals/src/main/java/ua/lviv/lgs/periodicals/service/BucketService.java
@@ -1,5 +1,7 @@
 package ua.lviv.lgs.periodicals.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
@@ -10,19 +12,23 @@ import java.util.List;
 
 @Service
 public class BucketService {
+    Logger logger = LoggerFactory.getLogger(BucketService.class);
 
     @Autowired
     private BucketRepository bucketRepository;
 
     public List<Bucket> getAll() {
+        logger.debug("Get all buckets items");
         return bucketRepository.findAll();
     }
 
     public void delete(Bucket bucket) {
+        logger.debug("Delete bucket item {} : " + bucket);
         bucketRepository.delete(bucket);
     }
 
     public Bucket add (Bucket bucket) {
+        logger.debug("Create new bucket item {} : "+ bucket);
         return bucketRepository.save(bucket);
     }
 }
